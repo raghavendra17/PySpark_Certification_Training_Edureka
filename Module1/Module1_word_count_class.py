fd6f39cabe7524c919f1bd8b043422322dcd68c0
Raghavendra <raghavendrareddy738@gmail.com>
2023-06-03 11:50:28 +0530

    adding files

---
+++ Module1/Module1_word_count_class.py
@@ -0,0 +1,14 @@
+import sys
+from pyspark import SparkContext, SparkConf
+
+class word():
+	if __name__ == "__main__":
+		conf = SparkConf().setAppName("Work Count in Pyspark")
+		sc = SparkContext(conf=conf)
+	
+		textFile = sc.textFile("/user/shubhamsag91edu/data/data/sample.txt")
+		rdd1 = textFile.flatMap(lambda line: line.split(" "))
+		rdd2 = rdd1.map(lambda word: (word,1))
+		rdd3 = rdd2.reduceByKey(lambda a,b: a+b)
+		rdd3.saveAsTextFile("/user/shubhamsag91edu/data/output/word_count_class")
+
